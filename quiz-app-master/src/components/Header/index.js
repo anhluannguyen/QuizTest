823b3f6e7b2e8a70a4c7ac057cb74ba32b7b4c90
luan <luan.na05062002@gmail.com>
2024-07-14 20:47:57 +0700

    hoan thien trang web

---
+++ quiz-app-master/src/components/Header/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Menu, Button } from 'semantic-ui-react';
+import logo from '../../images/logo1.png'
+
+const Header = () => {
+  const [promptEvent, setPromptEvent] = useState(null);
+  const [appAccepted, setAppAccepted] = useState(false);
+
+  let isAppInstalled = false;
+
+  if (window.matchMedia('(display-mode: standalone)').matches || appAccepted) {
+    isAppInstalled = true;
+  }
+
+  window.addEventListener('beforeinstallprompt', e => {
+    e.preventDefault();
+    setPromptEvent(e);
+  });
+
+  const installApp = () => {
+    promptEvent.prompt();
+    promptEvent.userChoice.then(result => {
+      if (result.outcome === 'accepted') {
+        setAppAccepted(true);
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+    });
+  };
+
+  return (
+    <Menu stackable inverted >
+      <Menu.Item header >
+        <div style={{display:'flex', gap:'8px'}}>
+          <img src={logo} style={{ height: 'auto', width:'80px' }} />
+          <h1>Quizz</h1>
+        </div>
+
+      </Menu.Item>
+      {promptEvent && !isAppInstalled && (
+        <Menu.Item position="right">
+          <Button
+            color="teal"
+            icon="download"
+            labelPosition="left"
+            content="Install App"
+            onClick={installApp}
+          />
+        </Menu.Item>
+      )}
+    </Menu>
+  );
+};
+
+export default Header;
